fa1785abbb7afe10b1730172c65fa3cb27e4b84e
Michael Bradnick <michael18781@gmail.com>
2021-02-21 13:24:14 +0000

    Functionality for multiple static masses. Thus added functionality for arbitrary forces with a new method.

--- Interaction.py
+++ Interaction.py
@@ -1,21 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
-import time
+plt.style.use('dark_background')
 G = 1
 
 
 class Interaction:
-    def __init__(self, mass1):
+    def __init__(self, masses):
         # Initialise the system with multiple rings of varying test particle density.
         # Initially, particles will be evenly spaced along the ring
 
-        self.M = mass1
+        self.masses = masses
         self.time_step = 0.1
-        self.steps = 10000
+        self.steps = 100000
 
         self.particle_density_factor = 1
-        self.radii_density = np.array([[2, 15], [3, 18], [4, 24], [5, 30], [6, 36]])
+        #[[2, 15], [3, 18], [4, 24], [5, 30], [6, 36]]
+        self.radii_density = np.array([[10,  1]])
 
         # Total number of particles
 
@@ -29,19 +30,36 @@ class Interaction:
             radius, particle_at_radius = radii[0], radii[1] * self.particle_density_factor
             for i in range(particle_at_radius):
                 # Finding the solution
-                solution = self.solving_odes(radius, 0, i * 2 * np.pi / particle_at_radius, np.sqrt(G * self.M / radius) / radius)
+                solution = self.solving_odes(radius, 0, i * 2 * np.pi / particle_at_radius, 0.1)
                 self.particle_solutions[particle_no] = solution
                 particle_no += 1
 
-    def force(self, r, theta):
-        return G * self.M/(r**2)
+    def accel(self, r, theta):
+        pos_x, pos_y = r*np.cos(theta), r*np.sin(theta)
+        accel_r, accel_theta = 0, 0
+        for mass in self.masses:
+            # Go through list of heavy masses and work out their contributions to the force on particles
+            x, y = mass[1], mass[2]
+            accel_mag = G * mass[0] / ((pos_x-x)**2 + (pos_y-y)**2)
+
+            l = r - x * np.cos(theta) - y * np.sin(theta)  # Geometry fun
+            d = x * np.sin(theta) - y * np.cos(theta)
+
+            cos_phi = l/np.sqrt(d**2+l**2)
+            sin_phi = d/np.sqrt(d**2+l**2)
+
+            accel_r -= accel_mag * cos_phi
+            accel_theta -= accel_mag * sin_phi
+
+        return accel_r, accel_theta
 
     def pair_odes(self, pos_vel, t):
         y0 = pos_vel[0]
         y1 = pos_vel[1]
         y2 = pos_vel[2]
         y3 = pos_vel[3]
-        return [y1, y0*y3**2 - self.force(y0, y2), y3, -2*y1*y3/y0]
+        # Divide by the radius in the angular force term so that we get the acceleration of theta
+        return [y1, y0*y3**2 + self.accel(y0, y2)[0], y3, -2*y1*y3/y0 + (1/y0)*self.accel(y0, y2)[1]]
 
     def solving_odes(self, r0, r_dot0, theta0, theta_dot0):
         times = np.arange(0, self.steps * self.time_step, self.time_step)
@@ -57,17 +75,24 @@ class Interaction:
 
     def plot_orbits(self):
         # Test with first particle
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(6, 4))
         self.produce_orbits()
         for particle in self.particle_solutions:
             r, theta = particle[1], particle[2]
-            plt.plot(r*np.cos(theta), r*np.sin(theta), label="", color='black')
+            ax.plot(r*np.cos(theta), r*np.sin(theta), color='red', lw=0.2)
+
+        # Styling
+        for mass in self.masses:
+            ax.scatter(mass[1], mass[2])
+        ax.set_xlabel('$x$')
+        ax.set_ylabel('$y$')
+        ax.set_title('Trajectories based on a central force at the origin for circular orbits.')
 
         plt.show()
 
 
 if __name__ == "__main__":
-    newInt = Interaction(1)
+    newInt = Interaction([(1, 0, 0), (3, 4, 0)])
     newInt.plot_orbits()
 
 
