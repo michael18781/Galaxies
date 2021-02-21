fe64bd895ed89f7d820d779c66bbbe76f9bbd322
Michael Bradnick <michael18781@gmail.com>
2021-02-21 11:20:12 +0000

    Updated array approach and plotted full trajectories for every particle found by solving the equations of motion (odeint) - better approach for the long run when this is needed as opposed to analytical solution.

--- Interaction.py
+++ Interaction.py
@@ -1,53 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import seaborn as sns
-sns.set_theme(style="whitegrid")
+import scipy.integrate as integrate
+import time
+G = 1
 
 
 class Interaction:
-    def __init__(self):
+    def __init__(self, mass1):
         # Initialise the system with multiple rings of varying test particle density.
         # Initially, particles will be evenly spaced along the ring
 
-        particle_density_factor = 1
-        self.anim_interval = 10
-        self.time_interval = self.anim_interval * 0.001 # Animation interval in ms
+        self.M = mass1
+        self.time_step = 0.1
+        self.steps = 10000
+
+        self.particle_density_factor = 1
         self.radii_density = np.array([[2, 15], [3, 18], [4, 24], [5, 30], [6, 36]])
 
         # Total number of particles
-        self.total_particles = int(particle_density_factor * np.sum(self.radii_density, axis=0)[1])
-        self.positions = np.zeros((self.total_particles, 3))  # Array for particle positions
-        self.velocities = np.zeros((self.total_particles, 3))  # Array for particle velocities
 
+        self.total_particles = int(self.particle_density_factor * np.sum(self.radii_density, axis=0)[1])
+        self.particle_solutions = np.zeros((self.total_particles, 5, self.steps))  # Array for particle positions
+
+    def produce_orbits(self):
         # Initialisation for circular orbits evenly spaced
         particle_no = 0
         for radii in self.radii_density:
-            radius, particle_at_radius = radii[0], radii[1] * particle_density_factor
+            radius, particle_at_radius = radii[0], radii[1] * self.particle_density_factor
             for i in range(particle_at_radius):
-                self.positions[particle_no] = np.array([radius, i * 2 * np.pi/particle_at_radius, 0])
-                self.velocities[particle_no] = np.array([0, np.sqrt(1 / radius), 0])
+                # Finding the solution
+                solution = self.solving_odes(radius, 0, i * 2 * np.pi / particle_at_radius, np.sqrt(G * self.M / radius) / radius)
+                self.particle_solutions[particle_no] = solution
                 particle_no += 1
 
-    def update_positions(self):
-        self.positions += self.velocities * self.time_interval
+    def force(self, r, theta):
+        return G * self.M/(r**2)
 
-    def plot(self, i):
-        # Function simply plots the position of every particle
-        plt.cla()
-        plt.title(f'Plot of test particle motions, T = {self.time_interval * i:.2f}s')
+    def pair_odes(self, pos_vel, t):
+        y0 = pos_vel[0]
+        y1 = pos_vel[1]
+        y2 = pos_vel[2]
+        y3 = pos_vel[3]
+        return [y1, y0*y3**2 - self.force(y0, y2), y3, -2*y1*y3/y0]
 
-        self.update_positions()
-        r = self.positions[:, 0]  # Current particle radii
-        theta = self.positions[:, 1]  # Current particle thetas
-        sns.scatterplot(x=r * np.cos(theta), y=r * np.sin(theta), color='g')  # Plot cartesian position
+    def solving_odes(self, r0, r_dot0, theta0, theta_dot0):
+        times = np.arange(0, self.steps * self.time_step, self.time_step)
+        # Solution takes the pair of odes, initial conditions, and an array of times
+        solution = integrate.odeint(self.pair_odes, [r0, r_dot0, theta0, theta_dot0], times)
 
-    def animate(self):
-        fig = plt.figure(figsize=(10, 10))
-        ani = FuncAnimation(fig, self.plot, interval=self.anim_interval)
+        # Extracting individual solutions
+        r = solution[:, 0]
+        r_dot = solution[:, 1]
+        theta = solution[:, 2]
+        theta_dot = solution[:, 3]
+        return np.array([times, r, theta, r_dot, theta_dot])
+
+    def plot_orbits(self):
+        # Test with first particle
+        fig, ax = plt.subplots()
+        self.produce_orbits()
+        for particle in self.particle_solutions:
+            r, theta = particle[1], particle[2]
+            plt.plot(r*np.cos(theta), r*np.sin(theta), label="", color='black')
+
         plt.show()
 
 
 if __name__ == "__main__":
-    newInt = Interaction()
-    newInt.animate()
+    newInt = Interaction(1)
+    newInt.plot_orbits()
+
+
